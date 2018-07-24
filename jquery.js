3fc6ce84f70068972844a344b5514c912094f68f
ashishmangukiya <ashishmangukiya97@gmail.com>
2018-07-24 14:02:23 +0530

    first commit

---
+++ jquery.js
@@ -0,0 +1,13 @@
+$(document).ready(function (){
+
+	$(".firstHeading").click(function(){
+
+		$(".firstParagraph").slideToggle("slow")});
+	$(".secondHeading").click(function(){
+
+		$(".secondParagraph").slideToggle("slow")});
+	$(".thirdHeading").click(function(){
+
+		$(".thirdParagraph").slideToggle("slow")});
+
+})
